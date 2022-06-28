05a891bb236d1bdf4693bdacb1cece37176aef14
ODN MAN <oduntane34.5@gmail.com>
2022-06-28 11:47:20 +0100

    Dotnet Pack

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mydotnetpacknugetpkg
+{
+    public class DempPackageDotnetPack 
+    {
+        public string HelloWorldNugetDemo() 
+        {
+            return "Hello world! Welcome to nuget packages with dotnet pack!";
+        }
+    }
+}
